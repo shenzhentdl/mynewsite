6a74eb886ea2b7d38ac7e4495289718420035026
shenzhentdl <3676537674@qq.com>
2024-07-17 12:41:12 +0800

    新增NewTable数据表

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class NewTable(models.Model):
+    bigint_f = models.BigIntegerField()
+    bool_f = models.BooleanField()
+    date_f = models.DateField(auto_now=True)
+    char_f = models.CharField(max_length=20, unique=True)
+    datetime_f = models.DateTimeField(auto_now_add=True)
+    decaimal_f = models.FloatField(null=True)
+    float_f = models.FloatField(null=True)
+    int_f = models.IntegerField(default=2010)
+    text_f = models.TextField()
