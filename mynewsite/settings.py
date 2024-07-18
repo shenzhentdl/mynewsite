7f788db8c71b0e5e716b68a7bb5cd0f5170852e1
shenzhentdl <3676537674@qq.com>
2024-07-18 18:36:56 +0800

    修改语言和时区

--- mynewsite/settings.py
+++ mynewsite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    'mysite',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "zh-hans"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Shagnhai"
 
 USE_I18N = True
 
